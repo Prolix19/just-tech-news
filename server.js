3e69d4e9903f7fc45b6e564f9bd71c29238797b0
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-03-23 14:50:48 -0500

    Hard-code secret string for Heroku functionality

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: mysecret,
+    secret: "mysecret",
     cookie: {},
     resave: false,
     saveUninitialized: true,
